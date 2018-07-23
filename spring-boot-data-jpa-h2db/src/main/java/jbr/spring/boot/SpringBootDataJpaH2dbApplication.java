4eee91b24428e5dccbcb3323bb482efac7c00751
Ranjith Sekar <java2ranjith@gmail.com>
2018-07-23 21:30:57 +0530

    updated

--- spring-boot-data-jpa-h2db/src/main/java/jbr/spring/boot/SpringBootDataJpaH2dbApplication.java
+++ spring-boot-data-jpa-h2db/src/main/java/jbr/spring/boot/SpringBootDataJpaH2dbApplication.java
@@ -1,7 +1,5 @@
 package jbr.spring.boot;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import jbr.spring.boot.model.Product;
 import jbr.spring.boot.service.ProductService;
 
 @SpringBootApplication
