6747582b4dcb2c00f37b8e34778417f9b18452cc
Ranjith Sekar <java2ranjith@gmail.com>
2020-02-29 14:25:10 +0530

    update

--- sboot-data-jpa-restapi/src/main/java/jbr/sboot/dao/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package jbr.sboot.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import jbr.sboot.model.Product;
-
-public interface ProductRepository extends CrudRepository<Product, String> {
-
-}
