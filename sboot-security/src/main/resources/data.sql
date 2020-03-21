3eae60887fd6052cb02f2d51dbc4c11cccf5837a
Ranjith Sekar <java2ranjith@gmail.com>
2020-03-21 15:55:59 +0530

    up

---
+++ sboot-security/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO springboot.app_user(id, username, password) VALUES (100,"ranjith","sekar");
+INSERT INTO springboot.app_user(id, username, password) VALUES (200,"spring","boot");
+INSERT INTO springboot.app_user(id, username, password) VALUES (300,"tom","cat");
---
+++ sboot-security/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE springboot.app_user
+(
+   id INTEGER,
+   username VARCHAR (30),
+   password VARCHAR (30)
+);
