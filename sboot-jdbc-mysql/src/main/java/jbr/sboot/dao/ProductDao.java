a238fe2a2b476a044dafad43d48c6c6b2cb4f38e
Ranjith Sekar <java2ranjith@gmail.com>
2020-03-21 14:30:36 +0530

    up

--- sboot-jdbc-mysql/src/main/java/jbr/sboot/dao/ProductDao.java
+++ sboot-jdbc-mysql/src/main/java/jbr/sboot/dao/ProductDao.java
@@ -3,8 +3,6 @@ package jbr.sboot.dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
